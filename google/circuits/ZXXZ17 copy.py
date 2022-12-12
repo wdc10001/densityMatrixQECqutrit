9c86a874d41ba385bb0473b31309824c33385dae
wdc <wdc@mail.ustc.edu.cn>
2022-12-13 03:08:05 +0800

    1

---
+++ google/circuits/ZXXZ17 copy.py
@@ -0,0 +1,56 @@
+import sys,os
+sys.path.append(os.path.abspath(''))
+from google.src.physicGate import *
+from google.src.config import *
+from numpy import random
+
+pattern1 = [[Q3,Q1],[Q11,Q6],[Q13,Q8],[Q7,Q2],[Q9,Q4],[Q16,Q14]]
+pattern2 = [[Q3,Q4],[Q11,Q12],[Q13,Q14],[Q7,Q6],[Q9,Q8],[Q16,Q15]]
+pattern3 = [[Q3,Q2],[Q5,Q4],[Q13,Q12],[Q7,Q8],[Q9,Q10],[Q0,Q1]]
+pattern4 = [[Q3,Q8],[Q5,Q10],[Q13,Q15],[Q7,Q12],[Q9,Q14],[Q0,Q2]]
+
+CZidle = lambda pattern:[i for i in Q_ALL if i not in [j for k in pattern for j in k]]
+
+#cross talk的pattern未写完
+crosstalk = lambda pattern,pCT:f'''
+GCT G{''.join([i[1:] for j in random.choice(6,2,replace=False) for i in pattern[j]])}
+''' if random.random() < pCT else ''
+
+EC = lambda tH,tCZ,pCT:f'''
+{B_ALL}
+{Y2M(Q_Anci)}
+{I(tH,Q_Data)}
+{B_ALL}
+{CZ(pattern1)}
+{crosstalk(pattern1,pCT)}
+{I(tCZ,CZidle(pattern1))}
+{B_ALL}
+{Y2M(Q_Data)}
+{I(tH,Q_Anci)}
+{B_ALL}
+{CZ(pattern2)}
+{crosstalk(pattern2,pCT)}
+{I(tCZ,CZidle(pattern2))}
+{B_ALL}
+{I(tH,Q_ALL)}
+{B_ALL}
+{CZ(pattern3)}
+{crosstalk(pattern3,pCT)}
+{I(tCZ,CZidle(pattern3))}
+{B_ALL}
+{Y2P(Q_Data)}
+{I(tH,Q_Anci)}
+{B_ALL}
+{CZ(pattern4)}
+{crosstalk(pattern4,pCT)}
+{I(tCZ,CZidle(pattern4))}
+{B_ALL}
+{Y2P(Q_Anci)}
+{I(tH,Q_Data)}
+'''
+
+ECM = lambda nD,tH,tCZ,tM,tR,pCT:EC(tH,tCZ,pCT) + f'''
+{B_ALL}
+{M_Anci}
+{DD(nD,tH,tM+tR,Q_Data)}
+'''
