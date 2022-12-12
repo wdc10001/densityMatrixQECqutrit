736d6124d39dbf7fb598fc31aed8c4bfba94718e
wdc <wdc@mail.ustc.edu.cn>
2022-12-12 22:34:38 +0800

    1

--- google/DEF/DEF7.py
+++ google/DEF/DEF7.py
@@ -14,7 +14,7 @@ d_4_qubit = [3]
 meas_qubit = d_2_qubit + d_4_qubit
 data_qubit = [1,2,4,5]
 
-data_def = [[0 for i2 in range(CycleNum-1)] for i1 in range(qnum)]
+data_def = [[0 for i2 in range(CycleNum)] for i1 in range(qnum)]
 with open(path,'r') as f:
     data_raw = [[ [] for i2 in range(qnum) ] for i1 in range(ExperimentNum)]
     for expi in range(ExperimentNum):
@@ -27,31 +27,31 @@ with open(path,'r') as f:
 
 fig = go.Figure()
 for ii in meas_qubit:
-    for cycle_num in range(CycleNum-1):
-        data_def[ii][cycle_num] = sum([int(data_raw[expi][ii][cycle_num] != data_raw[expi][ii][cycle_num+1]) for expi in range(ExperimentNum)])/ExperimentNum
+    for cycle_num in range(1,CycleNum):
+        data_def[ii][cycle_num] = sum([int(data_raw[expi][ii][cycle_num] != data_raw[expi][ii][cycle_num-1]) for expi in range(ExperimentNum)])/ExperimentNum
     data_def[ii][0] =  sum([int(data_raw[expi][ii][0] != 0) for expi in range(ExperimentNum)])/ExperimentNum
     DEF_std = np.std(data_def[ii][1:-1],ddof=1)
 
-data_def[0].append(sum([int(data_raw[expi][1][0]) ^ int(data_raw[expi][2][0]) for expi in range(ExperimentNum)])/ExperimentNum)
-data_def[3].append(sum([int(data_raw[expi][1][0]) ^ int(data_raw[expi][2][0]) ^ int(data_raw[expi][4][0]) ^ int(data_raw[expi][5][0]) for expi in range(ExperimentNum)])/ExperimentNum)
-data_def[6].append(sum([int(data_raw[expi][4][0]) ^ int(data_raw[expi][5][0]) for expi in range(ExperimentNum)])/ExperimentNum)
+data_def[0].append(sum([(int(data_raw[expi][1][0]) ^ int(data_raw[expi][2][0])) != data_raw[expi][0][CycleNum-1] for expi in range(ExperimentNum)])/ExperimentNum)
+data_def[3].append(sum([(int(data_raw[expi][1][0]) ^ int(data_raw[expi][2][0]) ^ int(data_raw[expi][4][0]) ^ int(data_raw[expi][5][0])) != data_raw[expi][3][CycleNum-1] for expi in range(ExperimentNum)])/ExperimentNum)
+data_def[6].append(sum([(int(data_raw[expi][4][0]) ^ int(data_raw[expi][5][0])) != data_raw[expi][6][CycleNum-1] for expi in range(ExperimentNum)])/ExperimentNum)
 
-d2_def = [0 for cycle_num in range(CycleNum)]
-for cycle_num in range(CycleNum):
+d2_def = [0 for cycle_num in range(CycleNum+1)]
+for cycle_num in range(CycleNum+1):
     for ii in d_2_qubit:
         d2_def[cycle_num] += data_def[ii][cycle_num]
     d2_def[cycle_num] /= len(d_2_qubit)
-d4_def = [0 for cycle_num in range(CycleNum)]
-for cycle_num in range(CycleNum):
+d4_def = [0 for cycle_num in range(CycleNum+1)]
+for cycle_num in range(CycleNum+1):
     for ii in d_4_qubit:
         d4_def[cycle_num] += data_def[ii][cycle_num]
     d4_def[cycle_num] /= len(d_4_qubit)
 
-fig.add_trace(go.Scatter(x=list(range(CycleNum)),y=data_def[0],mode='lines+markers',name=f'qubit{0}_std{round(DEF_std,4)}'))
-fig.add_trace(go.Scatter(x=list(range(CycleNum)),y=data_def[3],mode='lines+markers',name=f'qubit{3}_std{round(DEF_std,4)}'))
-fig.add_trace(go.Scatter(x=list(range(CycleNum)),y=data_def[6],mode='lines+markers',name=f'qubit{6}_std{round(DEF_std,4)}'))
-fig.add_trace(go.Scatter(x=list(range(CycleNum)),y=d2_def,mode='lines+markers',name=f'd2_qubit'))
-fig.add_trace(go.Scatter(x=list(range(CycleNum)),y=d4_def,mode='lines+markers',name=f'd4_qubit'))
+fig.add_trace(go.Scatter(x=list(range(CycleNum+1)),y=data_def[0],mode='lines+markers',name=f'qubit{0}_std{round(DEF_std,4)}'))
+fig.add_trace(go.Scatter(x=list(range(CycleNum+1)),y=data_def[3],mode='lines+markers',name=f'qubit{3}_std{round(DEF_std,4)}'))
+fig.add_trace(go.Scatter(x=list(range(CycleNum+1)),y=data_def[6],mode='lines+markers',name=f'qubit{6}_std{round(DEF_std,4)}'))
+fig.add_trace(go.Scatter(x=list(range(CycleNum+1)),y=d2_def,mode='lines+markers',name=f'd2_qubit'))
+fig.add_trace(go.Scatter(x=list(range(CycleNum+1)),y=d4_def,mode='lines+markers',name=f'd4_qubit'))
 
 fig.update_layout(xaxis=dict(tickmode='linear',dtick=1),xaxis_title='cycle',yaxis_title='DEF',title=title)
 fig.show()
