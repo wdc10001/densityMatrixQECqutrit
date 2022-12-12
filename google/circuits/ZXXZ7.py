2f826419971b97aa79ef66333cc32b114103bad2
wdc <wdc@mail.ustc.edu.cn>
2022-12-13 02:47:03 +0800

    1

---
+++ google/circuits/ZXXZ7.py
@@ -0,0 +1,52 @@
+import sys,os
+sys.path.append(os.path.abspath(''))
+from google.src.physicGate import *
+from google.src.config import *
+from numpy import random
+
+Q_ALL = [Q0,Q1,Q2,Q3,Q4,Q8,Q9]
+Q_Anci = [Q0,Q3,Q9]
+Q_Data = [Q1,Q2,Q4,Q8]
+M_Anci = ''.join([f'MEASURE {QR_Dict[i]} {i}\n' for i in Q_Anci])
+
+pattern1 = [[Q3,Q1],[Q9,Q4]]
+pattern2 = [[Q3,Q4],[Q9,Q8]]
+pattern3 = [[Q3,Q2],[Q0,Q1]]
+pattern4 = [[Q3,Q8],[Q0,Q2]]
+
+CZidle = lambda pattern:[i for i in Q_ALL if i not in [j for k in pattern for j in k]]
+
+EC = lambda tH,tCZ,pCT:f'''
+{B_ALL}
+{Y2M(Q_Anci)}
+{I(tH,Q_Data)}
+{B_ALL}
+{CZ(pattern1)}
+{I(tCZ,CZidle(pattern1))}
+{B_ALL}
+{Y2M(Q_Data)}
+{I(tH,Q_Anci)}
+{B_ALL}
+{CZ(pattern2)}
+{I(tCZ,CZidle(pattern2))}
+{B_ALL}
+{I(tH,Q_ALL)}
+{B_ALL}
+{CZ(pattern3)}
+{I(tCZ,CZidle(pattern3))}
+{B_ALL}
+{Y2P(Q_Data)}
+{I(tH,Q_Anci)}
+{B_ALL}
+{CZ(pattern4)}
+{I(tCZ,CZidle(pattern4))}
+{B_ALL}
+{Y2P(Q_Anci)}
+{I(tH,Q_Data)}
+'''
+
+ECM = lambda nD,tH,tCZ,tM,tR,pCT:EC(tH,tCZ,pCT) + f'''
+{B_ALL}
+{M_Anci}
+{DD(nD,tH,tM+tR,Q_Data)}
+'''
---
+++ google/run/runZXXZ7.py
@@ -0,0 +1,62 @@
+import sys,os
+sys.path.append(os.path.abspath(''))
+import cirq,time
+from google.src.qcisToCirq import qcisToCirq
+from google.circuits.ZXXZ7 import EC,ECM
+from google.src.config import *
+import multiprocessing
+from functools import partial
+import numpy as np
+from google.src.qubitPara import *
+from numpy import random
+
+Q_ALL = [Q0,Q1,Q2,Q3,Q4,Q8,Q9]
+Q_Data = [Q1,Q2,Q4,Q8]
+MZ = f'''
+{Y2M([Q2,Q4])}
+'''
+init_Z = f'''
+{Y2P([Q2,Q4])}
+'''
+def init_random()->str:
+    randomQubits = random.choice(Q_Data,random.choice(range(5),1),replace=False)
+    return f'''
+    {X2P(randomQubits)}
+    {X2P(randomQubits)}
+    '''
+
+M_ALL = ''.join([f'MEASURE {QR_Dict[i]} {i}\n' for i in Q_ALL])
+
+nD = 0
+M_Dire = MZ
+fHL = fWest
+qData = Q_Data
+
+pDict = {'px':px,'py':py,'pz':pz,'pM':pM,'pReset01':pReset01,'pReset02':pReset02,'pLeak':pLeak,'pCZ':pCZ,'pCT':pCT}
+tDict = {'T1_10':T1_10,'T1_21':T1_21,'Tp_10':Tp_10,'Tp_21':Tp_21,'Th12':Th12,'tH':tH,'tCZ':tCZ}
+
+start = time.time()
+def runCirc(ncycle:int,shots:int)->list:
+    # start = time.time()
+    qcis = f'{init_Z}'+ncycle*f'{ECM(nD,tH,tCZ,tM,tR,pCT)}'+f'{EC(tH,tCZ,pCT)}{M_Dire}{M_ALL}'
+    circuitList = qcisToCirq(qcis,qData,pDict,tDict,fHL,ten=False,eleven=False,circ=[]).matchline()
+    circuit = cirq.Circuit(circuitList)
+    sim = cirq.Simulator()
+    result = sim.simulate(circuit)
+    mDict = {i:result.measurements[i][0] for i in result.measurements}
+    mList = [mDict[key] for i in Q_ALL for key in mDict if key[:3] == i]
+    # print(time.time()-start)
+    if shots%1000 == 0: print('ncycle',ncycle,'shots',shots,'time',time.time()-start)
+    return mList
+
+if __name__ == '__main__':
+    # runCirc(8,0)
+    shots = 10000
+    pools = multiprocessing.Pool()
+    for ncycle in range(10,11):
+        result = pools.map(partial(runCirc,ncycle),range(shots))
+        np.savetxt(f'google/result/resultZXXZ7/qubit_initX_ncycle{ncycle+1}shots{shots}tH400pM0.03pCZ0.02pxyz0.01.txt',result,fmt='%d',delimiter='')
+    pools.close()
+    pools.join()
+
+#测试结果：2.3分钟一个周期
