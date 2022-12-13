8ce8c9e753550514e7bc78a262b438e3f0662b4c
wdc <wdc@mail.ustc.edu.cn>
2022-12-13 23:05:22 +0800

    1

--- google/run/runZXXZ17.py
+++ google/run/runZXXZ17.py
@@ -1,13 +1,13 @@
 import sys,os
 sys.path.append(os.path.abspath(''))
 import cirq,time
-from google.src.qcisToCirq import qcisToCirq
+from google.src.qcis_to_cirq import QcisToCirq
 from google.circuits.ZXXZ17 import EC,ECM
 from google.src.config import *
 import multiprocessing
 from functools import partial
 import numpy as np
-from google.src.qubitPara import *
+from google.src.qubit_para import *
 from numpy import random
 
 nD = 0
@@ -22,7 +22,7 @@ start = time.time()
 def runCirc(ncycle:int,shots:int)->list:
     # start = time.time()
     qcis = f'{init0}'+ncycle*f'{ECM(nD,tH,tCZ,tM,tR,pCT)}'+f'{EC(tH,tCZ,pCT)}{M_Dire}{M_ALL}'
-    circuitList = qcisToCirq(qcis,qData,pDict,tDict,fHL,ten=False,eleven=False,circ=[]).matchline()
+    circuitList = QcisToCirq(qcis,qData,pDict,tDict,fHL,ten=False,eleven=False,circ=[]).matchline()
     circuit = cirq.Circuit(circuitList)
     sim = cirq.Simulator()
     result = sim.simulate(circuit)
@@ -34,11 +34,11 @@ def runCirc(ncycle:int,shots:int)->list:
 
 if __name__ == '__main__':
     # runCirc(8,0)
-    shots = 10000
+    shots = 1600
     pools = multiprocessing.Pool()
     for ncycle in range(7,8):
         result = pools.map(partial(runCirc,ncycle),range(shots))
-        np.savetxt(f'google/result/resultZXXZ17/qubit_initZ_ncycle{ncycle+1}shots{shots}tH400pM0.03pCZ0.02pxyz0.01.txt',result,fmt='%d',delimiter='')
+        np.savetxt(f'google/result/resultZXXZ17/qubit_initZ_ncycle{ncycle+1}shots{shots}tH400pM0.01pCZ0.01pxyz0.01.txt',result,fmt='%d',delimiter='')
     pools.close()
     pools.join()
 
