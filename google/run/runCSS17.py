f6e72edff532bdef7abd48a687410d278cdee2a3
wdc <wdc@mail.ustc.edu.cn>
2022-12-18 02:38:58 +0800

    1

--- google/run/runCSS17.py
+++ google/run/runCSS17.py
@@ -1,15 +1,17 @@
 import sys,os
 sys.path.append(os.path.abspath(''))
 import cirq,time
-from google.src.qcisToCirq import qcisToCirq
+from google.src.qcis_to_cirq import QcisToCirq
 from google.circuits.CSS17 import EC,ECM
 from google.src.config import *
 import multiprocessing
 from functools import partial
 import numpy as np
-from google.src.qubitPara import *
+from google.src.qubit_para import *
 
 MZ = f'''
+'''
+init0 = f'''
 '''
 
 nD = 0
@@ -23,22 +25,22 @@ tDict = {'T1_10':T1_10,'T1_21':T1_21,'Tp_10':Tp_10,'Tp_21':Tp_21,'Th12':Th12,'tH
 start = time.time()
 def runCirc(ncycle:int,shots:int):
     # start = time.time()
-    qcis = f'{init_random}'+ncycle*f'{ECM(nD,tH,tCZ,tM,tR)}'+f'{EC(tH,tCZ)}{M_Dire}{M_ALL}'
-    circuitList = qcisToCirq(qcis,qData,pDict,tDict,fHL,ten=False,eleven=False,circ=[]).matchline()
+    qcis = f'{init0}'+ncycle*f'{ECM(nD,tH,tCZ,tM,tR)}'+f'{EC(tH,tCZ)}{M_Dire}{M_ALL}'
+    circuitList = QcisToCirq(qcis,qData,pDict,tDict,fHL,ten=False,eleven=False,circ=[]).matchline()
     circuit = cirq.Circuit(circuitList)
     sim = cirq.Simulator()
     result = sim.simulate(circuit)
     mDict = {i:result.measurements[i][0] for i in result.measurements}
     mList = [mDict[key] for i in Q_ALL for key in mDict if key[:3] == i]
     # print(time.time()-start)
-    if shots%1000 == 0: print('ncycle',ncycle,'shots',shots,'time',time.time()-start)
+    if (shots+1)%1000 == 0: print('ncycle',ncycle,'shots',shots,'time',time.time()-start)
     return mList
 
 if __name__ == '__main__':
-    runCirc(1,0)
-    shots = 40000
+    # runCirc(1,0)
+    shots = 10000
     pools = multiprocessing.Pool()
-    for ncycle in range(6,7):
+    for ncycle in range(7,8):
         result = pools.map(partial(runCirc,ncycle),range(shots))
         np.savetxt(os.path.abspath('')+'/google/result/resultCSS17/qubit_ncycle{ncycle+1}shots{shots}tH400pM0.03pCZ0.02pxyz0.01.txt',result,fmt='%d',delimiter='')
     pools.close()
