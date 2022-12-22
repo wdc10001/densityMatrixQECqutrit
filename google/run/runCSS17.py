2b84de0375339397a3723a83b09ada75b5f79c45
wdc <wdc@mail.ustc.edu.cn>
2022-12-22 14:58:40 +0800

    1

--- google/run/runCSS17.py
+++ google/run/runCSS17.py
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     pools = multiprocessing.Pool()
     for ncycle in range(7,8):
         result = pools.map(partial(runCirc,ncycle),range(shots))
-        np.savetxt(os.path.abspath('')+'/google/result/resultCSS17/qubit_ncycle{ncycle+1}shots{shots}tH400pM0.03pCZ0.02pxyz0.01.txt',result,fmt='%d',delimiter='')
+        np.savetxt(f'/google/result/resultCSS17/qubit_ncycle{ncycle+1}shots{shots}tH400pM0.03pCZ0.02pxyz0.01.txt',result,fmt='%d',delimiter='')
     pools.close()
     pools.join()
 
